6259f43f58e8a95fb42ed1c7a23364931da7dd7b
Jeremiah Martin <jer@jeremiahmartin.com>
2014-01-15 15:51:51 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
-scrupulous
-==========
+Scrupulous
+=======
 
-Simple inline form validation using HTML5 attributes that plays nicely with Bootstrap
+Scrupulous.js is a super simple inline form validation using HTML5 attributes. Add all the standard HTML5 form attributes and call the plugin and it will automatically add inline validation with full styleable elements and class names. 
+
+Scrupulous.js is built around [Bootstrap](http://getbootstrap.com/), using the same class name and HTML structure for simple implementation. Not using Bootstrap? No problem, just update the class names in the CSS file and with minor changes it should still work fine. 
+
+#Setup
+* scrupulous.css: you will need to include the scrupulous.css which adds some additional styling to the form elements. 
+* jQuery: Scrupulous should work with most newer versions of jQuery, Have not tested how far back it is supported
+* scrupulous.js: Runs a jQuery plugin. 
+
+	<script>
+		$(function(){
+			$('.my-form').scrupulous();	
+		});
+	</script>
+
+##HTML
+Then just add standard HTML5 attributes to your form and Scrupulous takes care of the rest. 
+
+	<div class="form-group">
+		<label for="email">Email</label>
+		<input type="email" class="form-control" id="email" name="email" title="Please Enter a Valid Email Address" required>
+    </div>
+
+Note that the title of the field becomes the error message, mimmicking the default browser HTML5 validation messaging.
+
+##HTML with Errors
+
+If an error is detected the resulting HTML is generated dynamically.
+
+	<div class="form-group has-error">
+		<label for="name">Name</label>
+		<input type="text" class="form-control invalid" id="name" name="name" title="Please Enter a Name" required>
+    	<label class="error-message" for="name">Please Enter a Name</label>
+    </div>
+
+##Valid HTML
+
+When the form validates the following HTML is generated dynamically 
+
+	<div class="form-group has-success">
+		<label for="name">Name</label>
+		<input type="text" class="form-control valid" id="name" name="name" title="Please Enter a Name" required>
+	</div>
+
---
+++ js/scrupulous.js
@@ -0,0 +1,173 @@
+(function ($) {
+  $.fn.scrupulous = function (callback) {
+    var $forms        = this,
+        $inputs       = $forms.find('select, input, textarea').not(':disabled'),
+        emailPattern  = "[^@]+@[^@]+\.[a-zA-Z]{2,6}",
+        $el,$form,$formGroup,elId,validity,errorMessage;
+    
+    $forms.find('input[type="email"]').attr('pattern',emailPattern);
+
+    $forms.attr('novalidate',true); //set all forms to novalidate to avoid default browser validation
+
+     
+
+    /*----------------------------------------------
+      equalTo(el);
+     function that checks if a value is equal to another value
+     based on the id value contained in data-equal-to attribue. 
+     ----------------------------------------------*/
+    var equalTo = function(el){
+      var equalToParentId = $(el).attr('data-equal-to');
+      
+      if($('#' + equalToParentId).length) {
+        // Compare to another input's value
+        if (el.value != $('#' + equalToParentId).val() || el.value === '') {
+          return false;
+        } else {
+          return true;
+        }
+      } else {
+        // Plain Object / Primitive evaluation
+        return (el.value == equalToParentId);
+      }
+    };
+
+    /*----------------------------------------------
+      checkboxValidity(el)
+      function to check if any checkboxes/radios are checked then validate that
+      section of the form 
+    ----------------------------------------------*/
+
+    var checkboxValidity = function(el){
+      var inputName = el.name,
+          isChecked = false;
+      $('input[name=' + inputName + ']').each(function(){
+        if(this.checked === true){
+          isChecked = true;
+        }
+      });
+
+      if(!isChecked && el.required === true){
+        return false;
+      }
+      else {
+        return true;
+      }
+    };
+
+    /*----------------------------------------------
+    setValid($el)
+    function that removes all invalid classes and 
+    error labels. 
+    ----------------------------------------------*/
+
+    var setValid = function($el) {
+      $el.addClass('valid');
+      $el.removeClass('invalid');
+      $formGroup = $el.parents('.form-group');
+      $formGroup.addClass('has-success');
+      $formGroup.removeClass('has-error');
+      $formGroup.find('.error-message').remove();
+    };
+
+    /*----------------------------------------------
+    setInvalid($el)
+    function that addes invalid classes and appends
+    error message labels to the parent div. 
+    ----------------------------------------------*/
+    var setInvalid = function($el) {
+      $el.addClass('invalid');
+      $el.removeClass('valid');
+      $formGroup =  $el.parents('.form-group');
+      $formGroup.addClass('has-error');
+      $formGroup.removeClass('has-success');
+      errorMessage = $el.attr('title');
+      //only append if there isn't one. helpful with radios and checkboxes
+      if($formGroup.find('.error-message').length === 0) {
+        $formGroup.append('<label class="error-message inactive" for="' + $el.attr('id') + '">' + errorMessage + '</label>');
+       
+      }
+      var t = setTimeout(function(){
+        $('.error-message').removeClass('inactive');
+      },10);
+    };
+
+    var validityChecker = function(el){
+      elValidity = el.checkValidity();
+      $el = $(el);
+
+      //if it is an equal-to check status
+      if($(el).hasClass('fn-equal-to')){
+        elValidity = equalTo(el);
+      }
+      
+      //if it is a not equal-to check status
+      if($(el).hasClass('fn-notequal-to')){
+        elValidity = !equalTo(el);
+      }
+
+      if($el.is(':checkbox') || $el.is(':radio')){
+        elValidity = checkboxValidity(el);
+      }
+
+
+      if(elValidity === true){
+        setValid($el);
+      }
+      else {
+        setInvalid($el);
+      }
+    };
+
+    //Check for has-success Validity on change/keyup
+      $inputs.on('change keyup',function(){
+        elValidity = this.checkValidity();
+        $el = $(this);
+
+        if($el.hasClass('fn-equal-to')){
+          elValidity = equalTo(this);
+        }
+        
+        if($el.hasClass('fn-notequal-to')){
+          elValidity = !equalTo(this);
+        }
+
+        if($el.is(':checkbox') || $el.is(':radio')){
+          elValidity = checkboxValidity(this);
+        }
+
+        if(elValidity === true){
+          setValid($el);
+        }
+      });
+
+    //Check Validity on Blur
+      $inputs.on('blur',function(){
+        if($(this).val() !== '') {
+          validityChecker(this);
+        }
+      }); 
+
+    //Check Validity for all elements on submit
+      $forms.on('submit',function(e){
+        $form = $(this);
+
+        if( typeof callBack !== "function" ) { callBack = function(){};}
+        callBack.call(this);
+
+        $form.find('select, input, textarea').each(function(){
+          validityChecker(this);
+        });
+        if($form.find('.has-error').length > 0){
+           //don't submit
+          var errorScrollTop = $form.find('.has-error:first').offset().top - 100;
+          if(errorScrollTop < $(window).scrollTop()) {
+            $("html, body").animate({ scrollTop: errorScrollTop }, 300);
+          }
+          $form.find('.has-error .invalid:first').focus();
+
+          e.preventDefault();
+        }
+      });
+  };
+})( jQuery );
